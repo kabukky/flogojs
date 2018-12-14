be0a0f6945e43e8c48d17c9dcd852b3178dfa493
Hagelstein, Kai <kai.hagelstein@spiegel.de>
2018-12-14 17:13:48 +0100

    Initial commit

---
+++ activity.go
@@ -0,0 +1,66 @@
+package js
+
+import (
+	"fmt"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"github.com/robertkrimen/otto"
+)
+
+var activityLog = logger.GetLogger("activity-vijay-js")
+
+const (
+	ivInputVars = "jsInput"
+	ivJs        = "jsCode"
+	ovOutput    = "jsOutput"
+)
+
+type JSActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new AppActivity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &JSActivity{metadata: metadata}
+}
+
+// Metadata returns the activity's metadata
+func (a *JSActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements api.Activity.Eval - Execute JS code
+func (a *JSActivity) Eval(context activity.Context) (done bool, err error) {
+
+	inputVars, _ := context.GetInput(ivInputVars).(interface{})
+	jsCode, _ := context.GetInput(ivJs).(string)
+
+	activityLog.Debugf("JavaScript Input: %v", inputVars)
+	activityLog.Debugf("JavaScript Code: %s", jsCode)
+
+	vm := otto.New()
+
+	//Set Input Variable
+	vm.Set(ivInputVars, inputVars)
+
+	v, err := vm.Run(jsCode)
+	if err != nil {
+		return false, activity.NewError(fmt.Sprintf("Failed to execute JavaScript code due to error: %s", err.Error()), "", nil)
+	}
+
+	var jsOutput interface{}
+	// Look for jsOutput variable value
+	value, err := vm.Get(ovOutput)
+	if value.IsNull() || value.IsUndefined() {
+		// Set returned value
+		jsOutput, _ = v.Export()
+	} else {
+		// Set jsOutput variable value
+		jsOutput, _ = value.Export()
+	}
+	context.SetOutput(ovOutput, jsOutput)
+
+	activityLog.Debugf("JavaScript Output: %v", jsOutput)
+	return true, nil
+}
---
+++ activity_test.go
@@ -0,0 +1,88 @@
+package js
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/core/data"
+	"github.com/stretchr/testify/assert"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval_InputVars(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	inputVar := make(map[string]interface{}, 2)
+	inputVar["n1"] = 2
+	inputVar["n2"] = 3
+	tc.SetInput(ivInputVars, inputVar)
+	tc.SetInput(ivJs, `jsInput.n1 + jsInput.n2`)
+	ok, err := act.Eval(tc)
+	assert.NoError(t, err)
+
+	if ok {
+		sum, _ := data.CoerceToInteger(tc.GetOutput(ovOutput))
+		assert.Equal(t, 5, sum)
+	}
+
+}
+
+func TestEval_OutputVars(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//Set input variables
+	inputVar := make(map[string]interface{}, 2)
+	inputVar["n1"] = 2
+	inputVar["n2"] = 3
+	tc.SetInput(ivInputVars, inputVar)
+
+	//Set JS code
+	tc.SetInput(ivJs, `var jsOutput = {}; jsOutput["sum"] = jsInput.n1 + jsInput.n2; jsOutput["result"] = "Sum is " + jsOutput["sum"];`)
+
+	success, err := act.Eval(tc)
+	assert.NoError(t, err)
+
+	if success {
+		output, ok := tc.GetOutput(ovOutput).(map[string]interface{})
+		assert.True(t, ok)
+
+		sum, _ := data.CoerceToInteger(output["sum"])
+		assert.Equal(t, 5, sum)
+
+		result, _ := data.CoerceToString(output["result"])
+		assert.Equal(t, "Sum is 5", result)
+	}
+}
